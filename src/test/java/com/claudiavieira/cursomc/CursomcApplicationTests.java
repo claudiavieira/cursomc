6b0fffe2beb0fe67ac55d852c2a03817854a5cbc
claudiavieira <claudia.araujo16@gmail.com>
2020-05-04 20:11:56 -0300

    Projeto foi criado

---
+++ src/test/java/com/claudiavieira/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudiavieira.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
